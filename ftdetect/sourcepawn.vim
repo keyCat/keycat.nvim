bed476a1436b2380876e301d86d927bad016259f
Ilya G <igorbachevsky@gmail.com>
2025-01-31 08:39:59 +0300

    use tree-sitter-sourcepawn

--- ftdetect/sourcepawn.vim
+++ ftdetect/sourcepawn.vim
@@ -1,1 +1,7 @@
-au BufRead,BufNewFile *.sp		set filetype=sourcepawn
+function! s:setf(filetype) abort
+    if &filetype !=# a:filetype
+        let &filetype = a:filetype
+    endif
+endfunction
+
+au BufNewFile,BufRead *.sp,*.sourcepawn,*.inc call s:setf('sourcepawn')
--- lua/plugins/nvim-treesitter.lua
+++ lua/plugins/nvim-treesitter.lua
@@ -5,7 +5,7 @@ return {
       local configs = require("nvim-treesitter.configs")
 
       configs.setup({
-          ensure_installed = { "c", "cmake", "comment", "cpp", "c_sharp", "diff", "go", "lua", "vim", "vimdoc", "query", "elixir", "heex", "javascript", "typescript", "tsx", "html", "css", "scss", "sql", "ssh_config", "php", "prisma", "csv" },
+          ensure_installed = { "c", "cmake", "comment", "cpp", "c_sharp", "diff", "go", "lua", "vim", "vimdoc", "query", "elixir", "heex", "javascript", "typescript", "tsx", "html", "css", "scss", "sql", "ssh_config", "sourcepawn", "php", "prisma", "csv" },
           sync_install = false,
           highlight = { enable = true },
           indent = { enable = true },  
