fe68e72ff5e2cbe2f2e2a0c956dde48eeb91a7e5
Ilya G <igorbachevsky@gmail.com>
2025-04-10 07:59:23 +0300

    upd lua/config/filetypes.lua
    - Add autocommand with indentation settings for Go

--- lua/config/filetypes.lua
+++ lua/config/filetypes.lua
@@ -7,4 +7,16 @@ vim.filetype.add({
     -- Source (Valve) CFG
     cfg = "sourcecfg",
   },
+
+  filename = {
+    -- Go (support for go.mod)
+    ["go.mod"] = "go",
+  },
+})
+
+-- Go Indents
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "go",
+  -- Friendship ended with ❌ gofmt (width=8) ❌, now tab (width=4) is my best friend
+  command = "setlocal tabstop=4 shiftwidth=4 noexpandtab",
 })
