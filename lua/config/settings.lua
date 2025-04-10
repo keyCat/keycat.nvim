3e7c67b18379538ad64eb6ee63d642e542edc106
Ilya G <igorbachevsky@gmail.com>
2025-04-10 04:25:40 +0300

    upd listchars settings

--- lua/config/settings.lua
+++ lua/config/settings.lua
@@ -2,8 +2,10 @@
 pcall(vim.cmd, "language en_US")    -- Set default editor language (wrapped in a pcall to swallow a error when given language is not available)
 
 -- Editor settings
-vim.opt.number = true               -- Always display line numbers (:set nonumber to disable)
+vim.opt.number = true               -- Always display line numbers (:vim.opt.nonumber to disable)
 vim.opt.expandtab = true            -- Use spaces for indentation instead of tabs
 vim.opt.tabstop = 2                 -- Number of spaces that a <Tab> in the file counts for.
 vim.opt.shiftwidth = 2              -- Number of spaces to use for each step of (auto)indent. 
 vim.opt.autoindent = false          -- Copy indent from current line when starting a new line (typing <CR> in Insert mode or when using the "o" or "O" command). 
+vim.opt.list = true                 -- Make tabs, spaces, trailing blanks and non-breakable characters visible
+vim.opt.listchars = { tab = "▓░", lead = "·", trail = "·", extends = "»", precedes = "«", nbsp = "⣿" }
