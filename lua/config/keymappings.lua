d2b1a115e48bcc3ddd18f3856f50406aabbb2288
Ilya G <igorbachevsky@gmail.com>
2025-04-10 06:23:58 +0300

    upd keymappings
    - Change vertical motions to allow soft-wraps navigation
    - Add mappings to move current line vertically using C-j, C-k

--- lua/config/keymappings.lua
+++ lua/config/keymappings.lua
@@ -1,6 +1,18 @@
 -- Set mapleader to Space
 vim.g.mapleader = ' '
- 
+
+-- Better vertical motions
+vim.keymap.set({ 'n', 'x' }, 'k', 'v:count == 0 ? "gk" : "k"', { desc = "Up", expr = true, silent = true })
+vim.keymap.set({ 'n', 'x' }, 'j', 'v:count == 0 ? "gj" : "j"', { desc = "Down", expr = true, silent = true })
+vim.keymap.set({ 'n', 'x' }, '<Up>', 'v:count == 0 ? "gk" : "k"', { desc = "Up", expr = true, silent = true })
+vim.keymap.set({ 'n', 'x' }, '<Down>', 'v:count == 0 ? "gj" : "j"', { desc = "Down", expr = true, silent = true })
+
+-- Move lines vertically wihtout the need to yank / dd them
+vim.keymap.set('n', '<C-j>', '<cmd>execute "move .+" . v:count1<cr>==', { desc = "Move Line Down" })
+vim.keymap.set('n', '<C-k>', '<cmd>execute "move .-" . (v:count1 + 1)<cr>==', { desc = "Move Line Up" })
+vim.keymap.set('v', '<C-j>', ':<C-u>execute "\'<,\'>move \'>+" . v:count1<cr>gv=gv', { desc = "Move Line Down" })
+vim.keymap.set('v', '<C-k>', ':<C-u>execute "\'<,\'>move \'<-" . (v:count1 + 1)<cr>gv=gv', { desc = "Move Line Up" })
+
 -- Telescope keymaps
 local telescope = require('telescope.builtin')
 vim.keymap.set('n', '<leader>ff', telescope.find_files, { desc = '(Telescope) Find files' })
